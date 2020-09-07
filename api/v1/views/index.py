6c4ca4d6c3a422649d769e4302ce96d3564182f6
lancewburklin <1677@holbertonschool.com>
2020-09-07 17:53:10 +0000

    Adds status method to count objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,16 @@ from flask.json import jsonify
 def status():
     """ returns status of API """
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats', methods=['GET'])
+def counter():
+    """ Returns the number of instances for each object type """
+    from models import storage
+    new_dict = {}
+    new_dict.update({"amenities": storage.count("Amenity")})
+    new_dict.update({"cities": storage.count("City")})
+    new_dict.update({"places": storage.count("Place")})
+    new_dict.update({"reviews": storage.count("Review")})
+    new_dict.update({"states": storage.count("State")})
+    new_dict.update({"users": storage.count("User")})
+    return jsonify(new_dict)
